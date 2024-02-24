9e78a064855f21c09ed24cc9b6e1e6fb848ff7eb
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-02-25 05:22:04 +0900

    remove useless import

--- composeApp/src/commonMain/kotlin/dev/nunu/compose/feature/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/nunu/compose/feature/home/HomeScreen.kt
@@ -1,7 +1,6 @@
 package dev.nunu.compose.feature.home
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
