baa9e6131f2681a7995a9c0f7b5acfee44f75e84
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-02-26 04:21:36 +0900

    Replace MaterialTheme with TodoTheme

--- composeApp/src/commonMain/kotlin/dev/nunu/compose/App.kt
+++ composeApp/src/commonMain/kotlin/dev/nunu/compose/App.kt
@@ -1,13 +1,13 @@
 package dev.nunu.compose
 
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
+import dev.nunu.compose.designsystem.TodoTheme
 import dev.nunu.compose.feature.home.HomeScreen
 
 @Composable
 fun App() {
-    MaterialTheme {
+    TodoTheme {
         Navigator(screen = HomeScreen)
     }
 }
